4f476c1ada4225f40146781a272bcc348a64a668
lyraxihe <129889894+lyraxihe@users.noreply.github.com>
2023-05-02 17:21:20 +0200

    pasé el codigo de "background" a "variables globales" (no funciona todavía(

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class Background : MonoBehaviour
 {
-    public GameObject CombatPosition;   // Prefab CombatPosition
-    public GameObject PrefabCombate;     // Prefab sala 1 - combate
-    public GameObject PrefabTienda;     // Prefab sala 2 - tienda
-    //public GameObject PrefabCofre;      // Prefab sala 3 - cofre
-    public GameObject PrefabJefe; // Prefab sala del medio - jefe
-    public RectTransform canvas;
+    //public GameObject CombatPosition;   // Prefab CombatPosition
+    //public GameObject PrefabCombate;     // Prefab sala 1 - combate
+    //public GameObject PrefabTienda;     // Prefab sala 2 - tienda
+    ////public GameObject PrefabCofre;      // Prefab sala 3 - cofre
+    //public GameObject PrefabJefe; // Prefab sala del medio - jefe
+    //public RectTransform canvas;
 
 
     // - - - - - - - - - - - - - - - - - - lyrita intento - - - - - - - - - - - - - - - - - - - - - - - - - - - //
@@ -21,17 +21,17 @@ public class Background : MonoBehaviour
     // - - - - - - - - - - - - - - - - - - lyrita intento - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
 
-    private float[] PositionsX = { 0, -3.5f, 0, 3.5f, 0 }; // Array de posiciones Coordenadas X del prefab CombatPosition
-    private float[] PositionsY = { 3.5f, 0, 0, 0, -3.5f };  // Array de posiciones Coordenadas Y del prefab CombatPosition
-    private GameObject[] Positions;                                                          // Array de prefabs CombatPosition
+    //private float[] PositionsX = { 0, -3.5f, 0, 3.5f, 0 }; // Array de posiciones Coordenadas X del prefab CombatPosition
+    //private float[] PositionsY = { 3.5f, 0, 0, 0, -3.5f };  // Array de posiciones Coordenadas Y del prefab CombatPosition
+    //private GameObject[] Positions;                                                          // Array de prefabs CombatPosition
 
     // Start is called before the first frame update
     void Start()
     {
-        Positions = new GameObject[PositionsX.Length]; // Crea el array de prefabs CombatPosition en base al tamaño del array de Coordenadas
+        //Positions = new GameObject[PositionsX.Length]; // Crea el array de prefabs CombatPosition en base al tamaño del array de Coordenadas
 
-        SetPositions(PositionsX.Length);              // Coloca las posiciones donde irán los personajes en pantalla
-        SetRooms(5);                             // Coloca los enemigos en las posiciones del combate
+        //SetPositions(PositionsX.Length);              // Coloca las posiciones donde irán los personajes en pantalla
+        //SetRooms(5);                             // Coloca los enemigos en las posiciones del combate
         //RefreshUI();
 
     }
@@ -81,15 +81,15 @@ public class Background : MonoBehaviour
     }
     // - - - - - - - - - - - - - - - - - - lyrita intento - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
-    private void SetPositions(int numPositions)
-    {
-        for (int i = 0; i < numPositions; i++) // Bucle for desde 0 al número de posiciones que debe haber para ir colocándolas
-        {
-            GameObject clon = Instantiate(CombatPosition);                           // Crea un clon del prefab CombatPosition
-            clon.transform.position = new Vector3(PositionsX[i], PositionsY[i], -1); // Coloca el clon en la primera posición que hay en los arrays de Coordenadas
-            Positions[i] = clon;                                                     // Añade el clon al array de prefabs CombatPosition
-        }
-    }
+    //private void SetPositions(int numPositions)
+    //{
+    //    for (int i = 0; i < numPositions; i++) // Bucle for desde 0 al número de posiciones que debe haber para ir colocándolas
+    //    {
+    //        GameObject clon = Instantiate(CombatPosition);                           // Crea un clon del prefab CombatPosition
+    //        clon.transform.position = new Vector3(PositionsX[i], PositionsY[i], -1); // Coloca el clon en la primera posición que hay en los arrays de Coordenadas
+    //        Positions[i] = clon;                                                     // Añade el clon al array de prefabs CombatPosition
+    //    }
+    //}
 
     /****************************************************************************************
      * Función: SetEnemies                                                                  *
@@ -99,58 +99,58 @@ public class Background : MonoBehaviour
      *      - maxEnemies: Número máximo de enemigos que habrá en el combate                 *
      * Return: Nada                                                                         *
      ****************************************************************************************/
-    private void SetRooms(int salas)
-    {
-        GameObject position, position_aux;
-        int RoomType;                                         // Almacena el tipo de sala                                // Coordenadas de una posición
-        int[] RoomsAmount = { 3, 1,};
+    //private void SetRooms(int salas)
+    //{
+    //    GameObject position, position_aux;
+    //    int RoomType;                                         // Almacena el tipo de sala                                // Coordenadas de una posición
+    //    int[] RoomsAmount = { 3, 1,};
 
-        position_aux = Positions[2];
+    //    position_aux = Positions[2];
 
-        if (position_aux.GetComponent<RoomsPosition>().Occupied == false)
-        {
-            position = Positions[2];
-            GameObject clon = Instantiate(PrefabJefe);             // Crea un clon del prefab jefe
-            clon.transform.parent = canvas;
-            clon.transform.position = Camera.main.WorldToScreenPoint(position.transform.position);   //cambia de posición de mundo a posición de cámara          
-            // Coloca el clon en la posición escogida
-            position_aux.GetComponent<RoomsPosition>().Occupied = true;
-            position.GetComponent<RoomsPosition>().Occupied = true; // Cambia esa posición a "ocupada"
-        }
+    //    if (position_aux.GetComponent<RoomsPosition>().Occupied == false)
+    //    {
+    //        position = Positions[2];
+    //        GameObject clon = Instantiate(PrefabJefe);             // Crea un clon del prefab jefe
+    //        clon.transform.parent = canvas;
+    //        clon.transform.position = Camera.main.WorldToScreenPoint(position.transform.position);   //cambia de posición de mundo a posición de cámara          
+    //        // Coloca el clon en la posición escogida
+    //        position_aux.GetComponent<RoomsPosition>().Occupied = true;
+    //        position.GetComponent<RoomsPosition>().Occupied = true; // Cambia esa posición a "ocupada"
+    //    }
 
-        for (int i = 0; i < salas - 1; i++) // Bucle for desde 0 hasta el número de salas que hay en el nivel
-        {
-            do
-            {
-                RoomType = Random.Range(0, 2); // Elige aleatoriamente el tipo de sala para asignar
-            } while (RoomsAmount[RoomType] == 0);
+    //    for (int i = 0; i < salas - 1; i++) // Bucle for desde 0 hasta el número de salas que hay en el nivel
+    //    {
+    //        do
+    //        {
+    //            RoomType = Random.Range(0, 2); // Elige aleatoriamente el tipo de sala para asignar
+    //        } while (RoomsAmount[RoomType] == 0);
 
-            do // Bucle While para controlar que la posición elegida aleatoriamente no esté ocupada, si lo está elige otra posición
-            {
-                position = Positions[Random.Range(0, Positions.Length)]; // Selecciona una posición al azar del array de prefabs CombatPosition
+    //        do // Bucle While para controlar que la posición elegida aleatoriamente no esté ocupada, si lo está elige otra posición
+    //        {
+    //            position = Positions[Random.Range(0, Positions.Length)]; // Selecciona una posición al azar del array de prefabs CombatPosition
 
-            } while (IsPositionOccupied(position)); // Si esa posición está ocupada (Occupied == true) vuelve a seleccionar otra, si no lo está, continua
+    //        } while (IsPositionOccupied(position)); // Si esa posición está ocupada (Occupied == true) vuelve a seleccionar otra, si no lo está, continua
 
 
 
-            if (RoomType == 0)                                           // Si la sala es combate
-            {
+    //        if (RoomType == 0)                                           // Si la sala es combate
+    //        {
 
-                GameObject clon = Instantiate(PrefabCombate);             // Crea un clon del prefab combate
-                clon.transform.parent = canvas;
-                clon.transform.position = Camera.main.WorldToScreenPoint(position.transform.position);                  // Coloca el clon en la posición escogida aleatoriamente
-                position.GetComponent<RoomsPosition>().Occupied = true; // Cambia esa posición a "ocupada"
-                RoomsAmount[0] -= 1;
-            }
-            else /*(RoomType == 1)*/                                     // Si la sala es tienda
-            {
+    //            GameObject clon = Instantiate(PrefabCombate);             // Crea un clon del prefab combate
+    //            clon.transform.parent = canvas;
+    //            clon.transform.position = Camera.main.WorldToScreenPoint(position.transform.position);                  // Coloca el clon en la posición escogida aleatoriamente
+    //            position.GetComponent<RoomsPosition>().Occupied = true; // Cambia esa posición a "ocupada"
+    //            RoomsAmount[0] -= 1;
+    //        }
+    //        else /*(RoomType == 1)*/                                     // Si la sala es tienda
+    //        {
 
-                GameObject clon = Instantiate(PrefabTienda);             // Crea un clon del prefab tienda
-                clon.transform.parent = canvas;
-                clon.transform.position = Camera.main.WorldToScreenPoint(position.transform.position);                  // Coloca el clon en la posición escogida aleatoriamente 
-                position.GetComponent<RoomsPosition>().Occupied = true; // Cambia esa posición a "ocupada"
-                RoomsAmount[1] -= 1;
-            }
+    //            GameObject clon = Instantiate(PrefabTienda);             // Crea un clon del prefab tienda
+    //            clon.transform.parent = canvas;
+    //            clon.transform.position = Camera.main.WorldToScreenPoint(position.transform.position);                  // Coloca el clon en la posición escogida aleatoriamente 
+    //            position.GetComponent<RoomsPosition>().Occupied = true; // Cambia esa posición a "ocupada"
+    //            RoomsAmount[1] -= 1;
+    //        }
             //else                                     // Si el enemigo elegido es un Slime
             //{
             //    GameObject clon = Instantiate(PrefabCofre);              // Crea un clon del prefab cofre
@@ -161,7 +161,7 @@ public class Background : MonoBehaviour
             //}
 
         }
-    }
+    
 
     /****************************************************************************************
      * Función: IsPositionOccupied                                                          *
@@ -172,14 +172,14 @@ public class Background : MonoBehaviour
      *      - True: Si la posición está ocupada                                             *
      *      - False: Si la posición está libre                                              *
      ****************************************************************************************/
-    private bool IsPositionOccupied(GameObject position)
-    {
-        if (position.GetComponent<RoomsPosition>().Occupied == true) // Si la variable "Occupied" es "true" es que está ocupada
-            return true;                                             // Devuelve "true" por que la posición está ocupada
+    //private bool IsPositionOccupied(GameObject position)
+    //{
+    //    if (position.GetComponent<RoomsPosition>().Occupied == true) // Si la variable "Occupied" es "true" es que está ocupada
+    //        return true;                                             // Devuelve "true" por que la posición está ocupada
 
-        return false;                                                // Devuelve "false" si "Occupied" es "false" y por tanto está libre
-    }
+    //    return false;                                                // Devuelve "false" si "Occupied" es "false" y por tanto está libre
+    //}
 
     
     
-}
+
--- Assets/Scripts/VariablesGlobales.cs
+++ Assets/Scripts/VariablesGlobales.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
+
 public class VariablesGlobales : MonoBehaviour
 {
     public static VariablesGlobales instance;
 
     public GameObject canvas;
     public GameObject canvasCreditos;
+    //public RectTransform _canvas;
+    public UnityEngine.Transform _canvas;
 
     //// MENÚ PAUSA
     //public GameObject PrefabMenuPausaBorder;
@@ -31,6 +34,16 @@ public class VariablesGlobales : MonoBehaviour
     //public GameObject ContinuarButtonText;
     //public GameObject SalirButton;
     //public GameObject SalirButtonText;
+
+    public GameObject RoomsPosition;          // Prefab RoomsPosition
+    public GameObject PrefabCombate;          // Prefab sala 1 - combate
+    public GameObject PrefabTienda;           // Prefab sala 2 - tienda
+    public GameObject PrefabJefe;             // Prefab sala del medio - jefe
+    
+    private float[] PositionsX = { 0, -3.5f, 0, 3.5f, 0 }; // Array de posiciones Coordenadas X del prefab RoomPosition
+    private float[] PositionsY = { 3.5f, 0, 0, 0, -3.5f };  // Array de posiciones Coordenadas Y del prefab RoomPosition
+    private GameObject[] Positions;           // Array de prefabs CombatPosition
+
     public bool EscPressed;
 
     // BOSS
@@ -84,6 +97,11 @@ public class VariablesGlobales : MonoBehaviour
 
     void Start()
     {
+        Positions = new GameObject[PositionsX.Length]; // Crea el array de prefabs CombatPosition en base al tamaño del array de Coordenadas
+
+        SetPositions(PositionsX.Length);              // Crea la posiciones en donde irá cada sala
+        SetRooms(5);                                  // Coloca cada sala en su posición dentro del mapa
+
         Boss = false;
         EscPressed = false;
 
@@ -144,47 +162,117 @@ public class VariablesGlobales : MonoBehaviour
         EscPressed = false;
         canvas.SetActive(!canvas.activeSelf);
     }
-    //public void MenuPausa()
-    //{
-    //    EscPressed = true;
-
-    //    //// MENU PAUSA
-    //    //ClonMenuPausaBorder = Instantiate(PrefabMenuPausaBorder);
-    //    //ClonMenuPausaBorder.transform.position = new Vector3(0, 0, -1);
-    //    //ClonMenuPausaBorder.transform.localScale = new Vector3(9, 10, 1);
-    //    ////ClonMenuPausaBorder.GetComponent<SpriteRenderer>().enabled = false;
-
-    //    //ClonMenuPausaContainer = Instantiate(PrefabMenuPausaContainer);
-    //    //ClonMenuPausaContainer.transform.position = new Vector3(0, 0, -2);
-    //    //ClonMenuPausaContainer.transform.localScale = new Vector3(8.5f, 9.5f, 1);
-    //    ////ClonMenuPausaContainer.GetComponent<SpriteRenderer>().enabled = false;
-
-    //    //PausaText = Instantiate(PrefabPausaText);
-    //    //PausaText.transform.parent = canvas.transform;
-    //    ////PausaText.GetComponent<TextMeshPro>().enabled = true;
-    //    //ContinuarButton = Instantiate(PrefabContinuarButton);
-    //    //ContinuarButton.transform.parent = canvas.transform;
-    //    ////ContinuarButton.GetComponent<Image>().enabled = true;
-    //    //ContinuarButtonText = Instantiate(PrefabContinuarButtonText);
-    //    //ContinuarButtonText.transform.parent = canvas.transform;
-    //    ////ContinuarButtonText.GetComponent<TextMeshPro>().enabled = true;
-    //    //SalirButton = Instantiate(PrefabSalirButton);
-    //    //SalirButton.transform.parent = canvas.transform;
-    //    ////SalirButton.GetComponent<Image>().enabled = true;
-    //    //SalirButtonText = Instantiate(PrefabSalirButtonText);
-    //    //SalirButtonText.transform.parent = canvas.transform;
-    //    ////SalirButtonText.GetComponent<TextMeshPro>().enabled = true;
-    //    canvas.GetComponent<Canvas>().enabled = true;
-    //}
-    public void SalirPausa()
+    private void SetPositions(int numPositions)
     {
-        EscPressed = false;
-        canvas.SetActive(!canvas.activeSelf);
-        SceneManager.LoadScene("MainMenu"); //abre la escena
+        for (int i = 0; i < numPositions; i++) // Bucle for desde 0 al número de posiciones que debe haber para ir colocándolas
+        {
+            GameObject clon = Instantiate(RoomsPosition);                           // Crea un clon del prefab CombatPosition
+            clon.transform.position = new Vector3(PositionsX[i], PositionsY[i], -1); // Coloca el clon en la primera posición que hay en los arrays de Coordenadas
+            Positions[i] = clon;                                                     // Añade el clon al array de prefabs CombatPosition
+        }
     }
+    private void SetRooms(int salas)
+    {
+        GameObject position, position_aux;
+        int RoomType;                                         // Almacena el tipo de sala                                // Coordenadas de una posición
+        int[] RoomsAmount = { 3, 1, };
 
-    ////private static Background instance = null;
-    ////public static Background Instance { get { return instance; } } //creo el Singletons
+        position_aux = Positions[2];
+
+        if (position_aux.GetComponent<RoomsPosition>().Occupied == false)
+        {
+            position = Positions[2];
+            GameObject clon = Instantiate(PrefabJefe);             // Crea un clon del prefab jefe
+            clon.transform.parent = _canvas;
+            clon.transform.position = Camera.main.WorldToScreenPoint(position.transform.position);   //cambia de posición de mundo a posición de cámara          
+            // Coloca el clon en la posición escogida
+            position_aux.GetComponent<RoomsPosition>().Occupied = true;
+            position.GetComponent<RoomsPosition>().Occupied = true; // Cambia esa posición a "ocupada"
+        }
+
+        for (int i = 0; i < salas - 1; i++) // Bucle for desde 0 hasta el número de salas que hay en el nivel
+        {
+            do
+            {
+                RoomType = Random.Range(0, 2); // Elige aleatoriamente el tipo de sala para asignar
+            } while (RoomsAmount[RoomType] == 0);
+
+            do // Bucle While para controlar que la posición elegida aleatoriamente no esté ocupada, si lo está elige otra posición
+            {
+                position = Positions[Random.Range(0, Positions.Length)]; // Selecciona una posición al azar del array de prefabs CombatPosition
+
+            } while (IsPositionOccupied(position)); // Si esa posición está ocupada (Occupied == true) vuelve a seleccionar otra, si no lo está, continua
+
+
+
+            if (RoomType == 0)                                           // Si la sala es combate
+            {
+
+                GameObject clon = Instantiate(PrefabCombate);             // Crea un clon del prefab combate
+                clon.transform.parent = _canvas;
+                clon.transform.position = Camera.main.WorldToScreenPoint(position.transform.position);                  // Coloca el clon en la posición escogida aleatoriamente
+                position.GetComponent<RoomsPosition>().Occupied = true; // Cambia esa posición a "ocupada"
+                RoomsAmount[0] -= 1;
+            }
+            else /*(RoomType == 1)*/                                     // Si la sala es tienda
+            {
+
+                GameObject clon = Instantiate(PrefabTienda);             // Crea un clon del prefab tienda
+                clon.transform.parent = _canvas;
+                clon.transform.position = Camera.main.WorldToScreenPoint(position.transform.position);                  // Coloca el clon en la posición escogida aleatoriamente 
+                position.GetComponent<RoomsPosition>().Occupied = true; // Cambia esa posición a "ocupada"
+                RoomsAmount[1] -= 1;
+            }
+            bool IsPositionOccupied(GameObject position)
+            {
+                if (position.GetComponent<RoomsPosition>().Occupied == true) // Si la variable "Occupied" es "true" es que está ocupada
+                    return true;                                             // Devuelve "true" por que la posición está ocupada
+
+                return false;                                                // Devuelve "false" si "Occupied" es "false" y por tanto está libre
+            }
+            //public void MenuPausa()
+            //{
+            //    EscPressed = true;
+
+            //    //// MENU PAUSA
+            //    //ClonMenuPausaBorder = Instantiate(PrefabMenuPausaBorder);
+            //    //ClonMenuPausaBorder.transform.position = new Vector3(0, 0, -1);
+            //    //ClonMenuPausaBorder.transform.localScale = new Vector3(9, 10, 1);
+            //    ////ClonMenuPausaBorder.GetComponent<SpriteRenderer>().enabled = false;
+
+            //    //ClonMenuPausaContainer = Instantiate(PrefabMenuPausaContainer);
+            //    //ClonMenuPausaContainer.transform.position = new Vector3(0, 0, -2);
+            //    //ClonMenuPausaContainer.transform.localScale = new Vector3(8.5f, 9.5f, 1);
+            //    ////ClonMenuPausaContainer.GetComponent<SpriteRenderer>().enabled = false;
+
+            //    //PausaText = Instantiate(PrefabPausaText);
+            //    //PausaText.transform.parent = canvas.transform;
+            //    ////PausaText.GetComponent<TextMeshPro>().enabled = true;
+            //    //ContinuarButton = Instantiate(PrefabContinuarButton);
+            //    //ContinuarButton.transform.parent = canvas.transform;
+            //    ////ContinuarButton.GetComponent<Image>().enabled = true;
+            //    //ContinuarButtonText = Instantiate(PrefabContinuarButtonText);
+            //    //ContinuarButtonText.transform.parent = canvas.transform;
+            //    ////ContinuarButtonText.GetComponent<TextMeshPro>().enabled = true;
+            //    //SalirButton = Instantiate(PrefabSalirButton);
+            //    //SalirButton.transform.parent = canvas.transform;
+            //    ////SalirButton.GetComponent<Image>().enabled = true;
+            //    //SalirButtonText = Instantiate(PrefabSalirButtonText);
+            //    //SalirButtonText.transform.parent = canvas.transform;
+            //    ////SalirButtonText.GetComponent<TextMeshPro>().enabled = true;
+            //    canvas.GetComponent<Canvas>().enabled = true;
+            //}
+            void SalirPausa()
+            {
+                EscPressed = false;
+                canvas.SetActive(!canvas.activeSelf);
+                SceneManager.LoadScene("MainMenu"); //abre la escena
+            }
+
+            ////private static Background instance = null;
+            ////public static Background Instance { get { return instance; } } //creo el Singletons
+        }
+    }
 }
 
    
